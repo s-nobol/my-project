85c3a1ba13088250501ae5d5f88e2b78b8eb64e0
ka1301 <ka1301@outlook.jp>
2019-10-28 11:30:36 +0000

    Ddocsの追加

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,14 @@
 module.exports = {
 
-// https://81b82d2528fb47e99be94707cafd0296.vfs.cloud9.us-east-1.amazonaws.com/
- devServer: {
-  compress: true,
-   disableHostCheck: true
-
- }
-    //  publicPath: "./",
-    // "devServer" : {
-    //     public : '3.91.37.95:80',
-    // }
+    // https://81b82d2528fb47e99be94707cafd0296.vfs.cloud9.us-east-1.amazonaws.com/
+    devServer: {
+        compress: true,
+        disableHostCheck: true
+    
+    },
+    
+        publicPath: 'my-project',
+        outputDir: 'docs',
+        filenameHashing: false,
+        productionSourceMap: false,
 }
